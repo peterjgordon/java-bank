33b5617ac616cd1ed0b56e1a0b1b9baa4a1bb5e8
Daniel Penney-Gallagher <penneygallagher@gmail.com>
2017-10-05 10:09:30 +0000

    database

--- create.sql
+++ create.sql
@@ -2,26 +2,38 @@ DROP DATABASE bankData;
 CREATE DATABASE bankData;
 USE bankData;
 
-CREATE TABLE customers(
-    id
-    firstName
-    lastName
-    dateOfBirth
-    gender
-    address
-);
-
-CREATE TABLE IF NOT EXISTS address(
-    postcode varchar(8) NOT NULL PRIMARY KEY,
+CREATE TABLE postcodes(
+    postcode varchar(8) PRIMARY KEY,
     country varchar(20) NOT NULL,
     town varchar(20) NOT NULL,
     county varchar(20) NOT NULL,
 );
 
+CREATE TABLE customers(
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    firstName varchar(20) NOT NULL,
+    lastName varchar(20) NOT NULL,
+    dateOfBirth date NOT NULL,
+    gender char NOT NULL,
+    address varchar(20) NOT NULL,
+    phoneNumber varchar(12) NOT NULL,
+    email varchar(20) NOT NULL,
+    postcode varchar(8)  NOT NULL,
+    FOREIGN KEY (postcode) REFERENCES postcodes(postcode),
+);
+
 CREATE TABLE accounts(
-    accountNumber varchar(8) NOT NULL PRIMARY KEY,
+    accountNumber int(8) ZEROFILL AUTO_INCREMENT NOT NULL PRIMARY KEY,
     balance decimal(13,2) DEFAULT 0,
-    overdraft unsigned decimal(6,2) DEFAULT 0,
+    --overdraft unsigned decimal(6,2) DEFAULT 0,
+);
+
+CREATE TABLE customers_accounts(
+    customerID int,
+    accountNumber int,
+    PRIMARY KEY (customerID, accountNumber),
+    FOREIGN KEY (customerID) REFERENCES customers(id),
+    FOREIGN KEY (accountNumber) REFERENCES accounts(accountNumber),
 );
 
 /*CREATE TABLE deposits(
