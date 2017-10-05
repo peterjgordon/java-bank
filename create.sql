33456a991c866b2d09ade5008e76c6ddc1e44493
Daniel Penney-Gallagher <penneygallagher@gmail.com>
2017-10-05 09:09:23 +0000

    implemented address table

--- create.sql
+++ create.sql
@@ -1,22 +1,40 @@
 DROP DATABASE bankData;
 CREATE DATABASE bankData;
 USE bankData;
-CREATE TABLE accounts(
-    accountNumber varchar(20) NOT NULL PRIMARY KEY,
-    balance decimal(13,2) DEFAULT 0,
-    overdraft unsigned decimal DEFAULT 0,
+
+CREATE TABLE customers(
+    id
+    firstName
+    lastName
+    dateOfBirth
+    gender
+    address
 );
 
-CREATE TABLE deposits(
+CREATE TABLE IF NOT EXISTS address(
+    postcode varchar(8) NOT NULL PRIMARY KEY,
+    country varchar(20) NOT NULL,
+    town varchar(20) NOT NULL,
+    county varchar(20) NOT NULL,
+);
+
+CREATE TABLE accounts(
+    accountNumber varchar(8) NOT NULL PRIMARY KEY,
+    balance decimal(13,2) DEFAULT 0,
+    overdraft unsigned decimal(6,2) DEFAULT 0,
+);
+
+/*CREATE TABLE deposits(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     value unsigned decimal(13,2) NOT NULL,
-    accountNumber varchar(20) NOT NULL,
+    accountNumber varchar(8) NOT NULL,
     transferDate date,
 );
 
 CREATE TABLE withdrawals(
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    value unsigned decimal NOT NULL,
-    accountNumber varchar(20) NOT NULL,
+    value unsigned decimal(13,2) NOT NULL,
+    accountNumber varchar(8) NOT NULL,
     transferDate date,
-);
+);*/
+
